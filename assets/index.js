b3c6e226d989a0fa554f662c69068543cc57ad0d
Maryna <serdeshnayamarina@gmail.com>
2023-10-31 13:53:45 -0700

    " finished with the 5 day forecast, applied style"

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,23 @@ const searchBtn = document.getElementById("searchButton");
 const cityInput = document.getElementById('cityInput');
 const citiesDiv = document.getElementById('cities');
 const currentDiv = document.getElementById('current');
-//console.log(cityInput, typeof cityInput);
-searchBtn.addEventListener('click', searchCities);
+const futureDiv = document.getElementById('future');
+const historyUl = document.getElementById('history');
+// searchCities needs to be wrapped in an anon-function so it doesn't automatically get the event object passed to it
+searchBtn.addEventListener('click', ()=>{searchCities()});
 
 const myApiKey = "e9b1c62b40d4ec53f934011c833d6262";
 
+// https://openweathermap.org/api
+// https://openweathermap.org/forecast16
 // https://openweathermap.org/api/one-call-3#how
+
 const weatherURI = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myApiKey}&units=imperial`
+
+const icons = {
+    cloudy: `<i class="fa-solid fa-cloud"></i>`,
+    sunny: `<i class="fa-solid fa-sun"></i>`
+}
 
 /*
 ^ above statment is same as below
@@ -20,10 +30,10 @@ function weatherURI(lat, lon) {
 
 
 
-async function searchCities() {
+async function searchCities(query) {
     //console.log('A button was clicked');
     //console.log('You searched for ' + cityInput.value);
-    let cityInputValue = cityInput.value;
+    let cityInputValue = query || cityInput.value;
     addToSearchHistory(cityInputValue)
     // console.log("cityInputValue: ", cityInputValue)
     if (!cityInputValue) {
@@ -44,17 +54,44 @@ async function searchCities() {
     }
 }
 
-function addToSearchHistory(query) {
-    let savedHistory = JSON.parse(localStorage.getItem('concord-searchHistory') || "[]")
-    savedHistory.push(query)
-    localStorage.setItem('concord-searchHistory', JSON.stringify(savedHistory))
+function getSavedHistory() {
+    return JSON.parse(localStorage.getItem('concord-searchHistory') || "[]")
 }
 
+function addToSearchHistory(query) {
+    let savedHistory = getSavedHistory()
+    savedHistory.push(query)
+    if (savedHistory.length > 10) {
+        savedHistory.shift()
+    }
+    localStorage.setItem('concord-searchHistory', JSON.stringify(savedHistory))
+    renderSearchHistory()
+}
+
+function renderSearchHistory() {
+    let savedHistory = getSavedHistory()
+    historyUl.innerHTML = ""
+    savedHistory.forEach((query,i) => {
+        const li = document.createElement('li')
+        const historyBtn = document.createElement('button')
+        historyBtn.innerText = query
+        li.append(historyBtn)
+        historyUl.append(li)
+        historyBtn.addEventListener('click', ()=>{
+            searchCities(query)
+        })
+    })
+}
+
+renderSearchHistory()
+
 function displayCities(cities) {
+    currentDiv.innerHTML = ""
+    futureDiv.innerHTML = ""
     citiesDiv.innerHTML = ""
     cities.forEach(city => {
         citiesDiv.innerHTML += `
-            <div class="city">
+            <div class="city card">
                 <h2>${city.name}, ${city.state}, ${city.country}</h2>
                 <button onclick="displayCurrent(${city.lat}, ${city.lon})">Current</button>
                 <button onclick="displayFuture(${city.lat}, ${city.lon})">Future</button>
@@ -64,22 +101,60 @@ function displayCities(cities) {
 }
 
 async function displayCurrent(lat, lon) {
+    // Reset
     citiesDiv.innerHTML = ""
+
+    // Fetch
     let res = await fetch(weatherURI(lat, lon))
     let data = await res.json()
     console.log(data)
+
+    // 
     let current = data.list[0]
+    let currentWeather = current.weather[0]
+    console.log("currentWeather: ", currentWeather)
+    let cloudy = currentWeather.description.includes("cloud")
+    let icon = icons[cloudy ? "cloudy" : "sunny"]
+
     let date = dayjs(new Date(current.dt_txt)).format('YY.MM.DD hh:ss a')
     currentDiv.innerHTML = `
-        <h2>${data.city.name}</h2>
-        <p>Last updated: ${date}</p>
-        <p>Weather: ${current.weather[0].description}</p>
-        <p>Current temperature: ${current.main.temp}&deg;F</p>
-        <p>Wind speed: ${current.wind.speed} ${current.wind.deg}&deg;</p>
-        <p>Humidity: ${current.main.humidity}%</p>
+        <div class="card">
+            <h2>${data.city.name}</h2>
+            <p>Last updated: ${date}</p>
+            <p>Weather: ${currentWeather.description} ${icon}</p>
+            <p>Current temperature: ${current.main.temp}&deg;F</p>
+            <p>Wind speed: ${current.wind.speed} ${current.wind.deg}&deg;</p>
+            <p>Humidity: ${current.main.humidity}%</p>
+        </div>
     `
 }
 
 async function displayFuture(lat, lon) {
-    console.log(lat, lon)   
+    citiesDiv.innerHTML = ""
+    let res = await fetch(weatherURI(lat, lon))
+    let data = await res.json()
+    let days = []
+    for (let i = 0; i < 5; i++) {
+        days.push(data.list[i*8])
+        days[i].date = dayjs().add(i, 'day').format('YYYY.MM.DD')
+    } 
+    days.forEach(day => {
+        renderDaily(day)
+    })
+}
+
+function renderDaily(day) {
+    let currentWeather = day.weather[0]
+    console.log("currentWeather: ", currentWeather)
+    let cloudy = currentWeather.description.includes("cloud")
+    let icon = icons[cloudy ? "cloudy" : "sunny"]
+    futureDiv.innerHTML += `
+        <div class="daily card">
+            <h3>${day.date}</h3>
+            <p>Weather: ${day.weather[0].description} ${icon}</p>
+            <p>Current temperature: ${day.main.temp}&deg;F</p>
+            <p>Wind speed: ${day.wind.speed} ${day.wind.deg}&deg;</p>
+            <p>Humidity: ${day.main.humidity}%</p>
+        </div>
+    `
 }
